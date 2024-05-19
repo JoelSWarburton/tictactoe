6d1f6da44d726b117053c22d23c181cde0922fbe
Joel Warburton <Joel.s.warburton@gmail.com>
2024-05-20 00:51:58 +1000

    Add some basic functionality for game board

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-alert("Hello");
+function GameBoard() {
+  const rows = 3;
+  const columns = 3;
+  const gameBoard = [];
+
+  //Initialise the board
+  for (let i = 0; i < rows; i++) {
+    gameBoard.push([]);
+    for (let j = 0; j < columns; j++) {
+      gameBoard[i].push("");
+    }
+  }
+
+  const printBoard = () => {
+    console.log(gameBoard);
+  };
+
+  const addMarker = (row, column, marker) => {
+    if (checkSpaceFree(row, column)) {
+      gameBoard[row][column] = marker;
+      return true;
+    }
+    return false;
+  };
+
+  const checkSpaceFree = (row, column) => {
+    return gameBoard[row][column] === "";
+  };
+
+  return {
+    printBoard,
+    addMarker,
+  };
+}
